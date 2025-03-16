5ff6d02850d3d904f8c681987985b6ad1363c4b1
junker530 <ddr4646@gmail.com>
2025-03-17 07:31:43 +0900

    Initial Commit

---
+++ OoBEUI/OoBEUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OoBEUIApp.swift
+//  OoBEUI
+//
+//  Created by Shota Sakoda on 2025/03/17.
+//
+
+import SwiftUI
+
+@main
+struct OoBEUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OoBEUITests/OoBEUITests.swift
@@ -0,0 +1,17 @@
+//
+//  OoBEUITests.swift
+//  OoBEUITests
+//
+//  Created by Shota Sakoda on 2025/03/17.
+//
+
+import Testing
+@testable import OoBEUI
+
+struct OoBEUITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
